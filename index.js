71996d1f20d02c25d0fda503b49e8657cf48ec83
Joana Castañon <113373883+joanacastanon@users.noreply.github.com>
2023-06-22 12:44:12 -0300

    update 22-06

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+/* aplicar código abaixo se o objetivo for fazer sumir a barra + texto (não apenas a barra)
+
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop) {
+        document.getElementById("barra-topo").style.top = "0";
+    } else {
+        document.getElementById("barra-topo").style.top = "-100px";
+    }
+}
+*/
