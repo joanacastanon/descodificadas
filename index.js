97bc8b3ca84cb0fb41e4c7ea8fc81d9d6fbdad3a
Joana Castañon <113373883+joanacastanon@users.noreply.github.com>
2023-06-22 15:57:22 -0300

    finalmente foi a navbar

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+/* window.addEventListener("scroll", removeBackgroundFunction) = 
+    removeBackgroundFunction();
+
+function removeBackgroundFunction() {
+    document.getElementById("barra-topo").style.removeProperty("background-color")
+} */
+
+/* aplicar código abaixo se o objetivo for fazer sumir a barra + texto (não apenas a barra) */
 
 
-
-/* aplicar código abaixo se o objetivo for fazer sumir a barra + texto (não apenas a barra)
-
-window.onscroll = function() {
+/* window.onscroll = function() {
     scrollFunction();
 }
 
@@ -13,5 +18,17 @@ function scrollFunction() {
     } else {
         document.getElementById("barra-topo").style.top = "-100px";
     }
-}
-*/
+} */
+
+window.onscroll = function addBackgroundFunction() {
+    /* var scroll = window.scrollY; */
+    var barraTopo = document.getElementById("barra-topo");
+
+    if (document.documentElement.scrollTop) {
+        
+        barraTopo.style.backgroundColor = "rgb(94, 86, 86)";
+    //abaixo nao funciona - ver como tirar a cor quando sobe de novo
+    } else {
+        barraTopo.style.removeProperty("background-color");
+    }
+ }
