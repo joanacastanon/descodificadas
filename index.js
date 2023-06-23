98574f65794ba876e591ba12bc98f593d7970ea5
Joana Castañon <113373883+joanacastanon@users.noreply.github.com>
2023-06-23 12:31:30 -0300

    update navbar

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-/* window.addEventListener("scroll", removeBackgroundFunction) = 
-    removeBackgroundFunction();
-
-function removeBackgroundFunction() {
-    document.getElementById("barra-topo").style.removeProperty("background-color")
-} */
-
-/* aplicar código abaixo se o objetivo for fazer sumir a barra + texto (não apenas a barra) */
-
-
-/* window.onscroll = function() {
-    scrollFunction();
-}
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop) {
-        document.getElementById("barra-topo").style.top = "0";
-    } else {
-        document.getElementById("barra-topo").style.top = "-100px";
-    }
-} */
-
 window.onscroll = function addBackgroundFunction() {
     /* var scroll = window.scrollY; */
-    var barraTopo = document.getElementById("barra-topo");
+    var navbar = document.getElementById("navbar");
 
     if (document.documentElement.scrollTop) {
-        
-        barraTopo.style.backgroundColor = "rgb(94, 86, 86)";
-    //abaixo nao funciona - ver como tirar a cor quando sobe de novo
+        navbar.style.backgroundColor = "rgba(79,71,78,1)";
     } else {
-        barraTopo.style.removeProperty("background-color");
+        navbar.style.removeProperty("background-color");
     }
  }
